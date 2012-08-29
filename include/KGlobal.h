cc96232c4a806b40235d5da902294971c7d6393f
Wang Guoyu <iosdevelop@me.com>
2012-08-29 15:54:40 +0800

    add some components size & rename the "macros"

--- include/KGlobal.h
+++ include/KGlobal.h
@@ -1,6 +1,12 @@
 #ifndef __KGlobal__
 #define __KGlobal__
 
+// Components size
+#define kSizeScreenWidth         320
+#define kSizeScreenHeight        480
+#define kSizeNaviBarHeight       44
+#define kSizeTabBarHeight        49
+#define kSizeStatusBarHeight     20
 
 #endif /* __KGlobal__ */
 
