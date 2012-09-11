69c8fa86cebabbcc1c59e43106c7e1109d664f61
Zhicheng Wei <zhicheng1988@gmail.com>
2012-09-11 14:19:48 +0800

    add useful macros

--- KickOffIOS/include/KGlobal.h
+++ KickOffIOS/include/KGlobal.h
@@ -1,12 +1,7 @@
 #ifndef __KGlobal__
 #define __KGlobal__
 
-// Components size
-#define kSizeScreenWidth         320
-#define kSizeScreenHeight        480
-#define kSizeNaviBarHeight       44
-#define kSizeTabBarHeight        49
-#define kSizeStatusBarHeight     20
+#include "KMarcos.h"
 
 #endif /* __KGlobal__ */
 
--- KickOffIOS/include/KMacros.h
+++ KickOffIOS/include/KMacros.h
@@ -14,6 +14,35 @@
 #define BFONT(size)     [UIFont boldSystemFontOfSize:(size)]
 #define BUNDLE(name)    [[NSBundle mainBundle] pathForResource:(name)
 
+// Components size
+#define kSizeScreenWidth         320
+#define kSizeScreenHeight        480
+#define kSizeNaviBarHeight       44
+#define kSizeTabBarHeight        49
+#define kSizeStatusBarHeight     20
+
+/* color helper */
+#define RGBACOLOR(c)			\
+	[UIColor colorWithRed:((c>>24)&0xFF)/255.0	\
+		green:((c>>16)&0xFF)/255.0	\
+		blue:((c>>8)&0xFF)/255.0	\
+		alpha:((c)&0xFF)/255.0]
+
+
+/* number helper */
+#define INT(a)		[NSNumber numberWithInt:(a)]
+#define STR(a)		[NSString stringWithFormat:@"%@", (a)]
+#define FLOAT(a)	[NSNumber numberWithFloat:(a)]
+
+#define NUMBER_OR_NIL(a)	\
+	(((a) && [(a) isKindOfClass:[NSNumber class]]) ? (a) : nil)
+
+#define STRING_OR_NIL(a)	\
+	(((a) && [(a) isKindOfClass:[NSString class]]) ? (a) : nil)
+
+#define STRING_OR_EMPTY(a)	\
+	(((a) && [(a) isKindOfClass:[NSString class]]) ? (a) : @"")
+
 
 #endif /* __KMacros __*/
 
